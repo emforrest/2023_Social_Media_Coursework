51c21ea4261df410649ad06cb218ab94d76b7658
emforrest <69385208+emforrest@users.noreply.github.com>
2023-03-03 15:59:38 +0000

    small changes

--- SocialMedia 2/src/socialmedia/SocialMedia.java
+++ SocialMedia 2/src/socialmedia/SocialMedia.java
@@ -238,7 +238,7 @@ public class SocialMedia implements SocialMediaPlatform {
 				if (a.getPost(id).getPostType().equals("EndorsementPost") || a.getPost(id).getPostType().equals("DeletedPost")){
 					throw new NotActionablePostException();
 				}
-				Stringbuilder str = showIndividualPost(id);
+				StringBuilder str = showIndividualPost(id);
 				StringBuilder postChildrenDetails = recursivePost(id, 0, str);
 				return postChildrenDetails;
 			}
@@ -247,7 +247,13 @@ public class SocialMedia implements SocialMediaPlatform {
 	}
 
 	private StringBuilder recursivePost(int id, int depth, StringBuilder postChildrenDetails){
-		postChildrenDetails.append("| ")
+		if (depth != 0){
+			for (Account a: Accounts){
+				if (a.hasPost(id)){
+					Post post = a.getPost(id);
+				}
+			}
+		}
 	}
 
 	@Override
