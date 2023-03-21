3655dc3b3aba819d46d17091c1b0ae769bc2e384
Jack Skinner <jskinner1119@gmail.com>
2023-03-21 15:06:01 +0000

    added doccument comments to comment and endorsemen

--- SocialMedia 2/src/socialmedia/Comment.java
+++ SocialMedia 2/src/socialmedia/Comment.java
@@ -1,15 +1,30 @@
 package socialmedia;
 
+/** 
+ * This class extends the Post superclass, used for posts of type CommentPost. Has it's additional attrubte originalPostId.
+ * 
+ * @author Jack Skinner, Eleanor Forrest
+ */
+
 public class Comment extends Post{
     private int originalPostID; 
 
+    /**
+     * constructor, creates an endorsement, given a message and orignalPostID. 
+     * @param message - String: the message that the comment will have.
+     * @param originalPostID int: The postid of the post this comment is commenting on. 
+     */
     public Comment(String message, int originalPostID) {
     super(message);
     this.originalPostID = originalPostID;
+    // as in endrosement, the posttype is set to OriginalPost in the superclass, this corrects it.
     this.postType = "CommentPost";
     }
 
-    //object getters
+    /**
+     * getter, returns the attribute OriginalPostId
+     * @return originalPostId - int: the ID of the post this comment has commented on.
+     */
     public int getOriginalPostID() {
         return originalPostID;
     }
--- SocialMedia 2/src/socialmedia/Endorsement.java
+++ SocialMedia 2/src/socialmedia/Endorsement.java
@@ -1,17 +1,32 @@
 package socialmedia;
 
+/**
+ * This class extends the Post superclass, used for posts of type EndorsementPost. Has it's additional attrubte originalPostId.
+ * 
+ * @author Jack Skinner, Eleanor Forrest
+ */
 public class Endorsement extends Post{
 
     private int originalPostId; 
 
+    /**
+     * constructor, creates an endorsement, given a message and orignalPostID. 
+     * @param message - String: Constructed and formatted in SocialMedia. the message this endorsement will have
+     * @param originalPostId - int: the id of the post that this endorsement is endorsing.
+     */
     public Endorsement(String message, int originalPostId) {
+        //calls the superclass, to set all of it's Post attributes like it's id.
         super(message); 
         this.originalPostId = originalPostId;
+        //it's postType is set to OriginalPost in the superclass, this corrects it. 
         this.postType = "EndorsementPost"; 
         
     }
 
-    //object getters 
+    /**
+     * getter, returns the attribute OriginalPostId
+     * @return originalPostId - int: the ID of the post this endorsement has endorsed.
+     */
     public int getOriginalPostId() {
         return originalPostId;
     }
